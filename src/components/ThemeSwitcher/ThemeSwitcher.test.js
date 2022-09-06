ffaa46ec7d1b9c9446fe8cbd390cb60340c8f848
Kasim Nadim <37031076+kasimnadim6@users.noreply.github.com>
2022-09-06 12:10:13 +0530

    enzyme

--- src/components/ThemeSwitcher/ThemeSwitcher.test.js
+++ src/components/ThemeSwitcher/ThemeSwitcher.test.js
@@ -1,5 +1,6 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { shallow } from 'enzyme';
 import ThemeSwitcher from './ThemeSwitcher';
 
 describe('ThemeSwitcher component', () => {
@@ -10,5 +11,32 @@ describe('ThemeSwitcher component', () => {
     expect(paraEl).toHaveTextContent(/light/i);
     fireEvent.click(switchThemeEl);
     expect(paraEl).toHaveTextContent(/dark/i);
+  });
+
+  // -------------------------------------------ENZYME-------------------------------------------
+  // The following tests use Enzyme with unofficial adapter
+  // you need to configure enzyme to use the adapter
+  // Refer: setupTests.js for it
+
+  it('should have para element', () => {
+    // shallow rendering will test only this component, not any child
+    const wrapper = shallow(<ThemeSwitcher />);
+    expect(wrapper.find('p')).toHaveLength(1);
+  });
+  it('should have anchor tag with text Refer Enzyme official document.', () => {
+    const wrapper = shallow(<ThemeSwitcher />);
+    expect(
+      wrapper.find('a').contains('Refer Enzyme official document.')
+    ).toEqual(true);
+  });
+  it('should have button with handler', () => {
+    const wrapper = shallow(<ThemeSwitcher />);
+    expect(wrapper.find('p').contains('light')).toEqual(true);
+
+    wrapper.find('button').simulate('click');
+    expect(wrapper.find('p').contains('dark')).toEqual(true);
+
+    wrapper.find('button').simulate('click');
+    expect(wrapper.find('p').contains('light')).toEqual(true);
   });
 });
